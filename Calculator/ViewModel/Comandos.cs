46f16fa287d59206c62b33ef052c829611d6fb45
Camavilca <orlando.camavilca@tecsup.edu.pe>
2019-10-28 17:09:07 -0500

    fix

--- Calculator/ViewModel/Comandos.cs
+++ Calculator/ViewModel/Comandos.cs
@@ -10,12 +10,40 @@ namespace Calculator.ViewModel
 {
     public class Comandos : ViewModelBase
     {
-        int currentState = 1;
-        string mathOperator;
-        double firstNumber, secondNumber;
 
-        string result;
-        public string Result
+        int currentState = 1;
+        String mathOperator;
+
+        double firstNumber;
+        public double FirstNumber
+        {
+            get { return firstNumber; }
+            set
+            {
+                if (firstNumber != value)
+                {
+                    firstNumber = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        double secondNumber;
+        public double SecondNumber
+        {
+            get { return secondNumber; }
+            set
+            {
+                if (secondNumber != value)
+                {
+                    secondNumber = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        String result;
+        public String Result
         {
             get { return result; }
             set
@@ -28,57 +56,119 @@ namespace Calculator.ViewModel
             }
         }
 
-        public ICommand Onseven { protected set; get; }
-        public ICommand Oneight { protected set; get; }
-        public ICommand Onnine { protected set; get; }
-        public ICommand Onfour { protected set; get; }
+        String operation;
+        public String Operation
+        {
+            get { return operation; }
+            set
+            {
+                if (operation != value)
+                {
+                    operation = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+
+        public ICommand OnClear { protected set; get; }
+        public ICommand OnCalculate { protected set; get; }
+        public ICommand OnSelectOperator { protected set; get; }
+        public ICommand NumericCommand { protected set; get; }
+
         public Comandos()
         {
 
-            Onseven = new Command(() => {
-                OnSelectNumber("7");
-            });
-            Oneight = new Command(() => {
-                OnSelectNumber("8");
-            });
-            Onnine = new Command(() => {
-                OnSelectNumber("9");
-            });
-            Onfour = new Command(() => {
-                OnSelectNumber("4");
+            Result = "0";
+            NumericCommand = new Command<String>(
+                 execute: (String parameter) =>
+                 {
+                     if (Result == "0" || currentState < 0)
+                     {
+                         Result = "";
+                         if (currentState < 0)
+                             currentState *= -1;
+                     }
+                     Result += parameter;
+
+                     double number;
+                     if (double.TryParse(Result, out number))
+                     {
+                         Result = number.ToString("N0");
+                         if (currentState == 1)
+                         {
+                             FirstNumber = number;
+                         }
+                         else
+                         {
+                             SecondNumber = number;
+                         }
+                     }
+
+                 });
+
+            OnSelectOperator = new Command<String>(
+                 execute: (String parameter) =>
+                 {
+                     if (parameter == "+")
+                     {
+                         FirstNumber = Int32.Parse(Result);
+                         Operation = "s";
+                     }
+                     if (parameter == "-")
+                     {
+                         FirstNumber = Int32.Parse(Result);
+                         Operation = "r";
+                     }
+                     if (parameter == "*")
+                     {
+                         FirstNumber = Int32.Parse(Result);
+                         Operation = "m";
+                     }
+                     if (parameter == "/")
+                     {
+                         FirstNumber = Int32.Parse(Result);
+                         Operation = "d";
+                     }
+
+                     currentState = -2;
+                     mathOperator = parameter;
+
+                 });
+
+            OnCalculate = new Command(() =>
+            {
+                SecondNumber = Int32.Parse(Result);
+                double resultOper = 0;
+
+                switch (Operation)
+                {
+                    case "s":
+                        resultOper = FirstNumber + SecondNumber;
+                        break;
+                    case "r":
+                        resultOper = FirstNumber - SecondNumber;
+                        break;
+                    case "m":
+                        resultOper = FirstNumber * SecondNumber;
+                        break;
+                    case "d":
+                        resultOper = FirstNumber / SecondNumber;
+                        break;
+                }
+                Result = resultOper + "";
+
             });
 
-           
+            OnClear = new Command(() =>
+            {
+                Result = "0";
+                FirstNumber = 0;
+                SecondNumber = 0;
+            });
+
 
         }
 
-
-        void OnSelectNumber(string ingresa)
-        {
-            if(this.Result == "0" || currentState < 0)
-            {
-                this.Result = "";
-                if (currentState < 0)
-                    currentState *= -1;
-            }
-
-            this.Result += ingresa;
-
-            double numero;
-            if (double.TryParse(this.Result , out numero))
-            {
-               // this.Result = numero.ToString("NO");
-                if(currentState == 1)
-                {
-                    firstNumber = numero;
-                }
-                else
-                {
-                    secondNumber = numero;
-                }
-            }
-
-        }
-        
     }
 }
